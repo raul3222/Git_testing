29e4c7157b7b0afd030f3cef59a2fe6633f2e73d
R.Shafigin <sh_raul08@mail.ru>
2021-11-10 18:50:43 +0200

    final

--- PersonsList/ViewControllers/DetailedContactListTableViewController.swift
+++ PersonsList/ViewControllers/DetailedContactListTableViewController.swift
@@ -10,7 +10,6 @@ import UIKit
 class DetailedContactListTableViewController: UITableViewController {
     
     private var contactList: [Person] = DataManager.shared
-    private var indexForContactList = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.rowHeight = 50
@@ -24,13 +23,11 @@ class DetailedContactListTableViewController: UITableViewController {
         return 2
     }
     
-   // Нашел ошибку в методе ниже после отправки задания на проверку.
-   //Пока что не смог решить проблему.
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "detailsCellId", for: indexPath)
         
-        let contact = contactList[indexForContactList]
+        let contact = contactList[indexPath.section]
         
         var content = cell.defaultContentConfiguration()
         
@@ -41,8 +38,6 @@ class DetailedContactListTableViewController: UITableViewController {
         } else {
             content.text = contact.email
             content.image = UIImage(named: "mail_logo1")
-
-            indexForContactList += 1
         }
         cell.contentConfiguration = content
         
